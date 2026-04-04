41309cbbb6bfbbee2d8d4972254d50a2ff4374e0
akthecoder123 <codingclashers@gmail.com>
2026-04-04 23:59:00 +0530

    Added API endoint, now all questions in the database will be displayed on '/questions' path

--- backend/apps/qp/views.py
+++ backend/apps/qp/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import JsonResponse
+from .models import Question
+
+def get_questions(request):
+    questions = Question.objects.all()
+
+    data = []
+    for q in questions:
+        data.append({
+            "id": q.id,
+            "subject": q.subject,
+            "chapter": q.chapter,
+            "question": q.question_text,
+            "options": {
+                "A": q.option_a,
+                "B": q.option_b,
+                "C": q.option_c,
+                "D": q.option_d,
+            },
+            "correct_answer": q.correct_answer,
+            "solution": q.solution,
+        })
+
+    return JsonResponse(data, safe=False)
--- backend/examera/urls.py
+++ backend/examera/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from apps.qp.views import get_questions
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('questions/', get_questions),
 ]
